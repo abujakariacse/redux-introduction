275dcc3e7623db30018eb5a35dd19da5b0aef0b1
Abu Jakaria <abujakaria316@gmail.com>
2024-02-04 23:54:26 +0600

    redux counter app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
+
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <>
-      <h1 className="text-red-500">Hello World</h1>
-    </>
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="flex items-center border p-16 bg-gray-200 rounded-md">
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-3 py-2 bg-green-500 text-white rounded-md"
+        >
+          Increment
+        </button>
+        <h1 className="mx-4 text-lg font-medium">Counter: {count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-3 py-2 bg-red-500 text-white rounded-md"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CounterReducer from "./features/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: CounterReducer,
+  },
 });
