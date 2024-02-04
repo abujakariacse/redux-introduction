e243b8ad94b603c048b53680d890f2e960efc002
Abu Jakaria <abujakaria316@gmail.com>
2024-02-05 01:10:59 +0600

    custom logger function

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./features/counterSlice";
+import Logger from "./middleware/Logger";
 
 export const store = configureStore({
   reducer: {
     counter: CounterReducer,
   },
+  //   devTools: false, // we will off the devtools on production
+  //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(Logger), // custom Logger Function
 });
 
 export type RootState = ReturnType<typeof store.getState>;
